7c45389daf852b1e7167d30aca16775481a47952
kod0751 <50727457+kod0751@users.noreply.github.com>
2020-10-23 22:29:54 +0900

    Initial commit

---
+++ dist/practice.js
@@ -0,0 +1,24 @@
+"use strict";
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
+const circle = new Circle(5);
+const rectangle = new Rectangle(3, 4);
+const shape = [circle, rectangle];
+shape.forEach(shape => {
+    console.log(shape.getArea());
+});
---
+++ src/practice.ts
@@ -0,0 +1,22 @@
+type Person {
+    name: string;
+    age?: number;
+};
+
+type Developer = Person & {
+    skills: string[];
+};
+
+const person: Person = {
+    name:'김사람',
+    age: 20,
+};
+
+const expert: Developer ={
+    name: '김개발',
+    skills: ['javascript', 'react', 'typescript']
+};
+
+type People = Person[];
+const people: People = [person, expert];
+
